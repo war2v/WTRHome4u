5cbb32e10e5791f4e58e5db8d22a554cdf0a9cf9
war2v <war2v@mtmail.mtsu.edu>
2025-05-19 13:37:14 -0500

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-300`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,88 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+import { Briefcase, House, Shield, Trees } from "lucide-react";
 import Image from "next/image";
+import { BsPeople, BsTools } from "react-icons/bs";
+import { CgCross } from "react-icons/cg";
+import { TbGrowth, TbTools } from "react-icons/tb";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="h-screen w-screen">
+      <nav className="flex gap-x-2 items-center m-2 p-2 bg-neutral-100 rounded-xl shadow-sm">
+        <div className="flex gap-x-2 hover:text-green-500 transition">
+          <House/>
+          <h1>WTR</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex w-full justify-end gap-x-2">
+          <Button>Tenants</Button>
+          <Button variant="outline">Owners</Button>
+        </div>
+      </nav>
+      
+      <Card className="m-2 p-2 rounded-xl shadow-sm h-full">
+        <CardHeader className="flex justify-center p-4">
+            <div className="flex flex-col gap-y-8 items-center rounded-lg p-4 my-4 w-3/4">
+              <h1 className="text-4xl">Faith-Driven Property Management Across Middle Tennessee</h1>
+              <p className="text-green-800">Residential & Commercial Rentals | Investor Services | 24/7 Maintenance</p>
+              <div className="border w-full border-neutral-300"/>
+              <h1 className="text-muted-foreground text-lg font-semibold">Come to us for your next home or need property management serices!</h1>
+              <div className="flex justify-center-center gap-x-4 py-4">
+                  <Button size="sm" className="text-sm">Properties</Button>
+                  <Button size="sm" className="text-sm">Tenant Application</Button>
+                  <Button size="sm" className="text-sm">Property Management</Button>
+              </div>
+              <div className="w-full flex justify-center gap-x-4 ">
+                
+                <Image src="/bg-nashsky.jpg" alt="nashville skyline" className="rounded-lg" width={500} height={500} />
+                <Image src="/bg-murf.jpg" alt="nashville skyline" className="rounded-lg" width={500} height={500} />
+              </div>
+            </div>
+          </CardHeader>
+        <CardContent>
+          <CardDescription>
+            
+            <div className="flex flex-col items-center gap-y-4  rounded-lg">
+              <div className="w-3/4">
+                <p className="text-lg">At WTR Capital, our mission is to deliver exceptional property management services rooted in integrity, faith, and community. We strive to enhance the value of real estate investments while creating quality living and working spaces that improve lives across Middle Tennessee</p>
+              </div>
+
+              <div className="border w-full border-neutral-300"/>
+              
+              <div className="flex flex-col items-center">
+                <h1 className="text-xl text-green-800">Our Values</h1>
+                <div className="flex gap-x-3">
+                  <div className="flex items-center gap-1">
+                    <Shield/>
+                    <h1>Integrity</h1>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <CgCross size={28}/>
+                    <h1>Faith</h1>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <BsPeople size={20}/>
+                    <h1>Community</h1>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <Trees />
+                    <h1>Growth</h1>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <TbTools size={19}/>
+                    <h1>Exellent Service</h1>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <Briefcase size={19}/>
+                    <h1>Profressionalism</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </CardDescription>
+        </CardContent>
+      </Card>
+      
     </div>
   );
 }
