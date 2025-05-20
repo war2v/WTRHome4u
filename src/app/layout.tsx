0b80abf5e47f4d4789398d32550009e1d9d80b79
war2v <war2v@mtmail.mtsu.edu>
2025-05-20 01:52:01 -0500

    Unused Imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Montserrat } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const montserrat = Montserrat({
   weight: "400",
